52c1c7143b82d3ee1a33c8293fd07f4ac60032b1
saranshdhingra <saranshdhingra@google.com>
2022-07-29 18:36:13 +0530

    Updated code(broken)

--- src/operations.php
+++ src/operations.php
@@ -4,7 +4,7 @@ namespace App;
 
 class Operations {
     public function multiply($val) {
-        if (gettype($val) !== 'integer' && gettype($val) !== 'double')
+        if (gettype($val) !== 'integer')
             throw new \Exception("Invalid types!");
             
         return $val * 2;
